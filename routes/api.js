129a6e174977863c23e2d9d95158c0998b2fa2cd
Nikita Inkin <nikitainkin@yahoo.com>
2020-04-07 00:23:15 +0300

    Fixed sending user info and 404 error

--- routes/api.js
+++ routes/api.js
@@ -15,8 +15,9 @@ router.get('/users/:id', (req, res) => {
   const user = users.find((x) => x._id === req.params.id);
   if (!user) {
     res.status(404).send({ message: 'Нет пользователя с таким id' });
+  } else {
+    res.send(user);
   }
-  res.send(user);
 });
 
 router.get('/:any', (req, res) => {
