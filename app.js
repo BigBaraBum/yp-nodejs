f821d5743a0b4a12bcff76658de3edb3c68d437d
Nikita Inkin <nikitainkin@yahoo.com>
2020-04-08 00:36:36 +0300

    hardcoded my id for authentification

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const { PORT = 3000 } = process.env;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+  req.user = {
+      _id: '5e8cefaab0d1134f106c5769' //Мой айди
+  };
+  next();
+});
 mongoose.connect('mongodb://localhost:27017/mestodb', {
     useNewUrlParser: true,
     useCreateIndex: true,
