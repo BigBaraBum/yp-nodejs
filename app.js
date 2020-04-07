22d04653ce6e064952c8241f4c7c13dbde58059c
Nikita Inkin <nikitainkin@yahoo.com>
2020-04-07 23:46:32 +0300

    added connection to db

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/api.js');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 const app = express();
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const users = require('../data/users.json');
 const cards = require('../data/cards.json');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 router.get('/users', (req, res) => {
   res.send(users);
@@ -11,7 +18,6 @@ router.get('/cards', (req, res) => {
 });
 
 router.get('/users/:id', (req, res) => {
-  // eslint-disable-next-line no-underscore-dangle
   const user = users.find((x) => x._id === req.params.id);
   if (!user) {
     res.status(404).send({ message: 'Нет пользователя с таким id' });
