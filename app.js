ce4a1b0b2db85a9606910237284cd53b0cceb381
Nikita Inkin <nikitainkin@yahoo.com>
2020-04-09 15:33:02 +0300

    changed routing in app.js due to separating routes in 2 files

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const path = require('path');
-const api = require('./routes/api.js');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cards = require('./routes/cards.js');
+const users = require('./routes/users.js');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -11,17 +11,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   req.user = {
-      _id: '5e8cefaab0d1134f106c5769' //Мой айди
+    _id: '5e8cefaab0d1134f106c5769', // Мой айди
   };
   next();
 });
 mongoose.connect('mongodb://localhost:27017/mestodb', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
-
-app.use('/', api);
+app.use('/cards', cards);
+app.use('/users', users);
+app.use('/:any', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT, () => {
 
