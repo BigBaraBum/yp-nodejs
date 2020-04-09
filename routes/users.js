b29093cd8830a4368d0d235e2495863d51c707a1
Nikita Inkin <nikitainkin@yahoo.com>
2020-04-09 18:55:15 +0300

    removed createUser route

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
 const {
-  createUser, getUsers, getUserById, updateProfile, updateAvatar,
+  getUsers, getUserById, updateProfile, updateAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
 
 router.get('/:userId', getUserById);
-
-router.post('/', createUser);
 
 router.patch('/me', updateProfile);
 
