5cdca98683387c1882725da05998979fb9f06ede
Nikita Inkin <nikitainkin@yahoo.com>
2020-04-09 14:45:58 +0300

    fixed eslint minor problems with comas and added avatar link validation

--- models/user.js
+++ models/user.js
@@ -5,18 +5,23 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 2,
-    maxlength: 30
+    maxlength: 30,
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    retuired: true
+    retuired: true,
   },
   avatar: {
     type: String,
-    required: true
-  }
+    required: true,
+    validate: {
+      validator(v) {
+        return /https?:\/\/(www\.)?(([a-z]+\.[a-z]+)|(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))(:\d{2,5})?([/a-z0-9]+#?)?/.test(v);
+      },
+    },
+  },
 });
 
 module.exports = mongoose.model('user', userSchema);
