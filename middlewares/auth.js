eeeb47a9c0d6b6a761aacd55e43cb091e67d5e07
Nikita Inkin <nikitainkin@yahoo.com>
2020-04-11 23:17:59 +0300

    changed auth middleware to extract token from cookie

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,13 +5,8 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
-  }
-  const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
   let payload;
-
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-secret');
   } catch (err) {
